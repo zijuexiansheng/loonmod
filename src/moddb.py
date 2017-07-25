a3950a8e660f7dd5e7b45b842954e02c6d03215b
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-24 22:18:50 -0400

    change local directory

--- src/moddb.py
+++ src/moddb.py
@@ -5,7 +5,7 @@ import os
 import argparse
 import sqlite3
 
-dirname = os.path.join( os.environ['LOONMOD_CONFIG'], "bash_func" )
+dirname = os.path.join( os.environ['LOONMOD_CONFIG'], "loonmod" )
 
 class Sqlite:
     def __init__(self):
